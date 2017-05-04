7a9fd9fb1315d22f51dcbe9c138e5a02fb7beb37
Victor <io@victorloux.uk>
2017-05-04 01:26:28 +0100

    Moving to gulp and Skeleton, initialised new structure

--- README.md
+++ README.md
@@ -2,8 +2,10 @@ My personal CV/portfolio, at https://victorloux.uk
 
 ## Build
 
-CSS dependencies: Sass, Bourbon and Neat.
-
-I used LiveReload. Call Jade/pug on `index.jade` and Sass on `css/main.sass`, all the other files are imported from these two.
+Run `npm install` then `gulp`.
 
 To convert images (same principle as [css-responsive-images](https://github.com/victorloux/css-responsive-images)), call `fish projects/convert.fish` (if you don't use fish, it can be trivially converted to a bash script).
+
+## Libraries
+
+The CSS and boilerplate is based off [Skeleton](https://github.com/WhatsNewSaes/Skeleton-Sass/), [FastShell](https://hosseinkarami.com/fastshell/), the [7-1 pattern](https://github.com/HugoGiraudel/sass-boilerplate), Bourbon and Neat.
---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+// Based off the Fastshell boilerplate
+
+var gulp         = require('gulp'),
+    gutil        = require('gulp-util'),
+    sass         = require('gulp-sass'),
+    pug          = require('gulp-pug'),
+    data         = require('gulp-data'),
+    browserSync  = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify       = require('gulp-uglify'),
+    rename       = require('gulp-rename'),
+    cssnano      = require('gulp-cssnano'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    fs           = require('fs'),
+    package      = require('./package.json');
+
+gulp.task('css', function () {
+    return gulp.src('src/sass/main.sass')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(cssnano())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('html', function () {
+    var dataFile = 'src/pages/projects.json';
+
+    return gulp.src('src/index.pug')
+    .pipe(data(function(file){
+        return JSON.parse(fs.readFileSync(dataFile));
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(pug({
+        pretty: true // @todo: remove in prod
+    }))
+    .pipe(gulp.dest('app/'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('js',function(){
+  gulp.src('src/js/smoothscroll.js')
+    .pipe(sourcemaps.init())
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(uglify())
+    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(browserSync.reload({stream:true, once: true}));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init(null, {
+        server: {
+            baseDir: "app"
+        }
+    });
+});
+
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['css', 'js', 'browser-sync'], function () {
+    gulp.watch("src/scss/**/*.scss", ['css']);
+    gulp.watch("src/js/*.js", ['js']);
+    gulp.watch("app/**/*.pug", ['html']);
+    gulp.watch("app/pages/projects.json", ['html']);
+});
